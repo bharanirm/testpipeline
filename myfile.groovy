bf233e61c5bc9a16233b166e7edcef86f85690e0
Bharani <bharanidharan.r@pactera.com>
2021-04-01 15:20:33 +0800

    jenkins file

--- myfile.groovy
+++ myfile.groovy
@@ -1,11 +1,3 @@
-class Demo {                                    
-    static void main(String) {          
-              myClosure()         
-    }
-     static def myClosure() {
         println("Welcome to Jenkins")
         println("Hellow World")
-        println("I'm Good")
-    
-    }
-}
+        println("I'm Good")   
