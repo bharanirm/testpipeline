8661874fb876de11922d8f63740076627e277e46
Bharani <bharanidharan.r@pactera.com>
2021-04-01 12:44:18 +0800

    Groovy file modified

--- myfile.groovy
+++ myfile.groovy
@@ -3,8 +3,8 @@ class Demo {
               myClosure()         
     }
      static def myClosure() {
-       println "My First Closure" 
-       println 'Groovy world!'
+        println ("My First Closure")
+        println 'Groovy world!'
         println("Hellow World")
         println("I'm Good")
         println("How are you")     
