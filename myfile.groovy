b9971ed227bda78bd4440ff4f516267b0a78d033
Bharani <bharanidharan.r@pactera.com>
2021-04-01 14:46:58 +0800

    Groovy file modified

--- myfile.groovy
+++ myfile.groovy
@@ -1,10 +1,10 @@
 class Demo {                                    
-    public void main(String... args) {          
+    static void main(String... args) {          
               myClosure()         
     }
      static def myClosure() {
-        println ("My First Closure")
-        println 'Groovy world!'
+        println("My First Closure")
+        println('Groovy world!')
         println("Hellow World")
         println("I'm Good")
         println("How are you")     
