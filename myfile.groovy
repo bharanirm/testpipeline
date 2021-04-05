3f05eda0599750581c74ac152a3c2f64da61d5c2
Bharani <bharanidharan.r@pactera.com>
2021-04-05 11:55:30 +0800

    Groovy Test

--- myfile.groovy
+++ myfile.groovy
@@ -1,3 +1,16 @@
-        println("Welcome to Jenkins")
-        println("Hellow World")
-        println("I'm Good")   
+
+def example1() {
+  println 'Hello from example1'
+  println("Hellow World")
+  println("I'm Good")
+  println("How are you")
+}
+
+def example2() {
+  println 'Hello from example2'
+  println("Hellow World")
+  println("I'm Good")
+  println("How are you")
+}
+
+return this
