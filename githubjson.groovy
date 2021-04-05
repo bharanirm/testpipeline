06ee0d265a62abd9c409574b7ee77a47f30b7568
Bharani <bharanidharan.r@pactera.com>
2021-04-05 14:49:12 +0800

    Groovy Test

--- githubjson.groovy
+++ githubjson.groovy
@@ -1,9 +1,10 @@
+/* import groovy.json.JsonSlurper
 String username = System.getenv('GITHUB_USERNAME')
 String password = System.getenv('GITHUB_PASSWORD')
 
-String GITHUB_API = 'https://github.com/bharanirm?tab=repositories'
+String GITHUB_API = 'https://api.github.com/respos'
 String repo = 'testpipeline'
-String PR_ID = '2' // Pull request ID
+String PR_ID = '1' // Pull request ID
 
 String url = "${GITHUB_API}/${username}/${repo}/pulls/${PR_ID}"
 println "Querying ${url}"
@@ -14,4 +15,19 @@ def bodyText = json.body
 // Check if Pull Request body has certain text
 if ( bodyText.find('Safari') ) {
     println 'Found Safari user'
-}
+} */
+import groovy.json.JsonSlurper
+
+def getPrBody(String githubUsername, String githubToken, String repo, String id) {
+  String GITHUB_API = 'https://api.github.com/repos'
+
+  String url = "${GITHUB_API}/${githubUsername}/${repo}/pulls/${id}"
+  println "Querying ${url}"
+  def text = url.toURL().getText(requestProperties: ['Authorization': "token ${githubToken}"])
+  def json = new JsonSlurper().parseText(text)
+  def bodyText = json.body
+
+  return bodyText
+  }
+
+return this
